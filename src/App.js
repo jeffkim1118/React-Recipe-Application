450c66a3797354c424f81ee6c95b4297d6747cfb
Jeffrey <jeffkim1118@outlook.com>
2022-01-16 20:25:42 -0500

    Testing connection with Spoonacular API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 
 function App() {
-  const queryRequest = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2`;
-  fetch(queryRequest)
-  .then((res) => res.json())
-  .then(data => console.log(data))
+  // const queryRequest = `https://api.spoonacular.com/food/products/search?query=pasta&apiKey=5c28e37ff52340769cc4e6978685e652`;
+  // fetch(queryRequest)
+  // .then((res) => res.json())
+  // .then(data => console.log(data))
   return (
     <div className="App">
       
