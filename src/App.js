cd82bbbe15e9e57c4e3747376fbf61505b587c5c
Jeffrey <jeffkim1118@outlook.com>
2022-01-16 20:14:55 -0500

    added few stuffs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 
 function App() {
+  const queryRequest = `https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2`;
+  fetch(queryRequest)
+  .then((res) => res.json())
+  .then(data => console.log(data))
   return (
     <div className="App">
       
