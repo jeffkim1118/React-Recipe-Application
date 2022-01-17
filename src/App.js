0bde2955df814d64834bedcb53daf73fdd250b3b
Jeffrey <jeffkim1118@outlook.com>
2022-01-17 11:05:37 -0500

    new components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 
 function App() {
-  // const queryRequest = `https://api.spoonacular.com/food/products/search?query=pasta&apiKey=5c28e37ff52340769cc4e6978685e652`;
-  // fetch(queryRequest)
-  // .then((res) => res.json())
-  // .then(data => console.log(data))
+  
   return (
     <div className="App">
       <SearchBar />
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,31 @@
 import React from "react";
 
 function SearchBar(){
-  // const queryRequest = `https://api.spoonacular.com/food/products/search?query=pasta&apiKey=5c28e37ff52340769cc4e6978685e652`;
+  // const queryRequest = `https://api.spoonacular.com/food/products/search?query=${query}&apiKey=5c28e37ff52340769cc4e6978685e652`;
   // fetch(queryRequest)
   // .then((res) => res.json())
   // .then(data => console.log(data))
+
+    const [query, setquery] = useState("");
+    const [recipes, setrecipes] = useState("");
+
     return(
         <div className="SearchBar">
             <form>
                 <label>
-                    Search Your Desired Recipes!
-                    <input type="text" name="Search"/>
+                    Search Your Desired Recipes!                  
                 </label>
+                <input type="text" 
+                    placeholder="Search" 
+                    value={query} 
+                    onChange={(e)=>setquery(e.target.value)}  
+                />
                 <input type="submit" value="Submit" />
             </form>
+            <div>
+
+            </div>
+
         </div>
     )
 }
