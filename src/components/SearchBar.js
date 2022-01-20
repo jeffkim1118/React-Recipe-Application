5c6cfc98fffc976f7c1f73201c95e9d58851f31f
Jeffrey <jeffkim1118@outlook.com>
2022-01-19 20:07:35 -0500

    added search function to find recipes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,44 @@
 import React from "react";
+import { useState } from "react";
 
 function SearchBar(){
-  // const queryRequest = `https://api.spoonacular.com/food/products/search?query=${query}&apiKey=5c28e37ff52340769cc4e6978685e652`;
-  // fetch(queryRequest)
-  // .then((res) => res.json())
-  // .then(data => console.log(data))
+  
+    const [query, setQuery] = useState("");
+    // const [recipes, setRecipes] = useState([]);
+    // const [alert, setAlert] = useState("");
+    
+    const queryRequest = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=5c28e37ff52340769cc4e6978685e652`;
 
-    const [query, setquery] = useState("");
-    const [recipes, setrecipes] = useState("");
+    const getData = () => {
+        if(query !== ""){
+            fetch(queryRequest)
+            .then((res) => res.json())
+            .then(data => console.log(data))
+            setQuery("");
+        }
+    }
+
+    const onChange = e => setQuery(e.target.value);
+
+    const onSubmit = e => {
+        e.preventDefault();
+        getData();
+    }
 
     return(
         <div className="SearchBar">
-            <form>
+            <form onSubmit={onSubmit}>
                 <label>
                     Search Your Desired Recipes!                  
                 </label>
                 <input type="text" 
                     placeholder="Search" 
-                    value={query} 
-                    onChange={(e)=>setquery(e.target.value)}  
+                    value={query}
+                    onChange={onChange}
                 />
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Search" />
             </form>
             <div>
-
             </div>
 
         </div>
