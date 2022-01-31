98fec78ddba71932a768df7765ba39389c025418
NYCDOE <admin@AFVFFL1W7Q6L7.local>
2022-01-31 14:24:08 -0500

    Created a fetch request on recipedetails components

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-function RecipeDetails({props}){
-    console.log(props)
+function RecipeDetails(){
+    let path = window.location.pathname;
+    let recipeId = path.substring(15);
     
+    const idquery = `https://api.spoonacular.com/recipes/${recipeId}/information?includeNutrition=false&apiKey=5c28e37ff52340769cc4e6978685e652`;
+
+    fetch(idquery)
+    .then(res => res.json())
+    .then(data => console.log(data));
+
     return(
         <div>
             <p>Hello</p>
